2467953b32682651144b50d31f24bc58e45b38f7
Eugene Molotov <molotov@it-projects.info>
2020-05-08 15:43:24 +0500

    fixup! :tada: :one::two: saas_access_apps_master

--- saas_access_apps_master/models/saas_db.py
+++ saas_access_apps_master/models/saas_db.py
@@ -10,22 +10,11 @@ class SaasDb(models.Model):
 
     installed_apps = fields.Many2many("ir.module.module", "saas_db_installed_apps_rel", readonly=1)
 
-    def write_values_to_build(self, build_env):
-        super(SaasDb, self).write_values_to_build(build_env)
+    def read_values_from_build(self):
+        vals = super(SaasDb, self).read_values_from_build()
 
-        # disallowing all users to install apps
-        build_env.ref('access_apps.group_allow_apps').write({'users': [(5,)]})
-
-    def read_values_from_build(self, build_env, vals):
-        super(SaasDb, self).read_values_from_build(build_env, vals)
-
-        installed_apps_names = list(map(
-            lambda item: item["name"],
-            build_env['ir.module.module'].search_read([
-                ("state", "=", "installed"),
-                ("application", "=", True),
-            ], ['name'])
-        ))
+        installed_apps_names = vals.pop("installed_apps_names")
         vals.update(
             installed_apps=[(6, 0, self.env["ir.module.module"]._search([("name", "in", installed_apps_names)]))]
         )
+        return vals
