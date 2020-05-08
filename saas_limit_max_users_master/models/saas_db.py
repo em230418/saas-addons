ffe8aa532961cc52abc7f643fdcff17e92eefd93
Eugene Molotov <molotov@it-projects.info>
2020-05-08 15:50:33 +0500

    saas_limit_max_users_master

--- saas_limit_max_users_master/models/saas_db.py
+++ saas_limit_max_users_master/models/saas_db.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import api, models, fields
 from odoo.exceptions import ValidationError
 
 
