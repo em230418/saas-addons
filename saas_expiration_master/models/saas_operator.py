c8579e63b12e180bdf0ef24609640061ac26471a
Eugene Molotov <molotov@it-projects.info>
2020-05-08 15:44:41 +0500

    fixup! :tada: :one::two: saas_expiration_master

--- saas_expiration_master/models/saas_operator.py
+++ saas_expiration_master/models/saas_operator.py
@@ -9,4 +9,4 @@ class SaasOperator(models.Model):
     _inherit = 'saas.operator'
 
     def get_mandatory_modules(self):
-        return super(SaasOperator, self).get_mandatory_modules() + ["database_expiration"]
+        return super(SaasOperator, self).get_mandatory_modules() + ["saas_expiration_build"]
