b9ae3f3c1b617332bdeb3874c7f76441b801a8ba
Eugene Molotov <molotov@it-projects.info>
2020-05-08 12:38:35 +0500

    :tada: :one::two: saas_build_base

---
+++ saas_build_base/__manifest__.py
@@ -0,0 +1,49 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License MIT (https://opensource.org/licenses/MIT).
+
+{
+    "name": """SaaS: Base module for builds""",
+    "summary": """This module is base module for builds""",
+    "category": "Hidden",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=12.0",
+    "images": [],
+    "version": "12.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "apps@it-projects.info",
+    "website": "https://apps.odoo.com/apps/modules/12.0/saas_build_base/",
+    "license": "MIT",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        'base',
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+
+    # "demo_title": "SaaS: Base module for builds",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "This module is base module for builds",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
