c1b6c981375a10b1acab2465d284fc05636e281f
Eugene Molotov <molotov@it-projects.info>
2020-05-13 11:02:27 +0500

    saas_limit_max_users_master/

--- saas_limit_max_users_master/models/saas_db.py
+++ saas_limit_max_users_master/models/saas_db.py
@@ -1,12 +1,8 @@
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-import logging
-
 from odoo import api, models, fields
 from odoo.exceptions import ValidationError
-
-_logger = logging.getLogger(__name__)
 
 
 class SaasDb(models.Model):
@@ -30,22 +26,21 @@ class SaasDb(models.Model):
 
         _, model, res_id = self.xmlid_lookup("access_limit_max_users.max_users_limit")
 
-        self.execute_kw(
-            model, "write", [res_id],
-            {"max_records": self.max_users_limit}
-        )
+        self.execute_kw(model, "write", [res_id], {"max_records": self.max_users_limit})
 
     def read_values_from_build(self):
         vals = super(SaasDb, self).read_values_from_build()
 
-        vals.update(
-            users_count=self.execute_kw("res.users", "search_count", [])
-        )
+        vals.update(users_count=self.execute_kw("res.users", "search_count", []))
 
         if not self.max_users_limit:
-            _, model, res_id = self.xmlid_lookup("access_limit_max_users.max_users_limit")
+            _, model, res_id = self.xmlid_lookup(
+                "access_limit_max_users.max_users_limit"
+            )
             vals.update(
-                max_users_limit=self.execute_kw(model, "search_count", [("id", "=", res_id)], ["max_records"])[0]["max_records"]
+                max_users_limit=self.execute_kw(
+                    model, "search_count", [("id", "=", res_id)], ["max_records"]
+                )[0]["max_records"]
             )
 
         return vals
