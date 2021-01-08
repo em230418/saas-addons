9ab3375e06906399e7816038b27cbcc206ae62c7
Eugene Molotov <molotov@it-projects.info>
2021-01-08 15:04:41 +0500

    saas_contract: вынесли какие-то вещи по автоудалению билдов

--- saas_contract/models/saas_db.py
+++ saas_contract/models/saas_db.py
@@ -1,8 +1,7 @@
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
-from datetime import timedelta
+from odoo import fields, models
 
 
 class SaasDb(models.Model):
@@ -37,46 +36,3 @@ class SaasDb(models.Model):
                 "default_line_recurrence": True,
             }
         }
-
-    # TODO: поправить
-
-    @api.model
-    def remove_long_expired_unpaid_builds(self):
-        long_expired_builds = self.env["saas.db"].search([
-            ("expiration_date", "<", fields.Date.today() - timedelta(days=7)),
-            ("type", "=", "build"),
-        ])
-
-        contracts = long_expired_builds.mapped("contract_id")
-
-        paid_invoices = self.env["account.move"].search([
-            ("invoice_payment_state", "=", "paid"),
-            ("contract_id", "in", contracts.ids)
-        ])
-
-        paid_contracts = paid_invoices.mapped("contract_id")
-        unpaid_contracts = contracts - paid_contracts
-
-        long_expired_unpaid_builds = unpaid_contracts.mapped('build_id')
-
-        unpaid_contracts.write({"active": False})
-        long_expired_unpaid_builds.unlink()
-
-    @api.model
-    def remove_long_expired_paid_builds(self):
-        long_expired_builds = self.env["saas.db"].search([
-            ("expiration_date", "<", fields.Date.today() - timedelta(days=90)),
-            ("type", "=", "build"),
-        ])
-        expired_contracts = long_expired_builds.mapped("contract_id")
-
-        expired_contracts.write({"active": False})
-        long_expired_builds.unlink()
-
-    @api.model
-    def remove_old_draft_builds(self):
-        self.env["saas.db"].search([
-            ("create_date", "<", fields.Date.today() - timedelta(days=1)),
-            ("type", "=", "build"),
-            ("state", "=", "draft"),
-        ]).unlink()
