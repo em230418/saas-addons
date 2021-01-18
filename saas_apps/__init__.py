5c7b862e63c0d3e73f52ec444b925974c39122e1
Eugene Molotov <molotov@it-projects.info>
2021-01-18 15:34:40 +0500

    saas_apps: Добавили поля и тесты

--- saas_apps/__init__.py
+++ saas_apps/__init__.py
@@ -1,2 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import controllers, models
+from . import controllers
+from . import models
+from . import tests
--- saas_apps/models/saas_app.py
+++ saas_apps/models/saas_app.py
@@ -14,7 +14,9 @@ class SaasApp(models.Model):
     icon_image = fields.Binary("Icon")
 
     allow_to_sell = fields.Boolean(default=True, string="Sellable")
-    product_tmpl_id = fields.Many2one("product.template")
+    product_tmpl_id = fields.Many2one("product.template", ondelete="cascade")
+    month_product_id = fields.Many2one("product.product", string="Product for monthly subscription", compute="_compute_product_ids", store=True)
+    year_product_id = fields.Many2one("product.product", string="Product for annually subscription", compute="_compute_product_ids", store=True)
     currency_id = fields.Many2one("res.currency", related="product_tmpl_id.currency_id")
 
     # TODO: when following fields are written, you need to update prices on product.product
@@ -22,6 +24,44 @@ class SaasApp(models.Model):
     year_price = fields.Float("Year price", default=0.0)
 
     # TODO: add non-storable compute field like "Are cyclic dependency detected?"
+
+    @api.depends("product_tmpl_id")
+    def _compute_product_ids(self):
+        patvs_month = self.env.ref("saas_product.product_attribute_value_subscription_monthly")
+        patvs_year = self.env.ref("saas_product.product_attribute_value_subscription_annually")
+        attr = self.env.ref("saas_product.product_attribute_subscription")
+
+        for app in self:
+            if not app.product_tmpl_id:
+                app.month_product_id = app.year_product_id = self.env["product.product"]
+                continue
+
+            line = self.env["product.template.attribute.line"].sudo().search([
+                ("product_tmpl_id", "=", app.product_tmpl_id.id),
+                ("attribute_id", "=", attr.id),
+            ])
+            if not line:
+                line = line.create({
+                    "product_tmpl_id": app.product_tmpl_id.id,
+                    "attribute_id": attr.id,
+                    "value_ids": [(6, 0, [
+                        patvs_year.id, patvs_month.id,
+                    ])]
+                })
+
+            ptv_ids = line.product_template_value_ids
+
+            month_ptv = ptv_ids.filtered(lambda x: x.product_attribute_value_id == patvs_month)
+            month_ptv.write({
+                "price_extra": app.month_price
+            })
+            app.month_product_id = month_ptv.ptav_product_variant_ids[:1]
+
+            year_ptv = ptv_ids.filtered(lambda x: x.product_attribute_value_id == patvs_year)
+            year_ptv.write({
+                "price_extra": app.year_price
+            })
+            app.year_product_id = year_ptv.ptav_product_variant_ids[:1]
 
     @api.model
     def create(self, vals):
@@ -62,6 +102,7 @@ class SaasApp(models.Model):
 
         return ",".join(make_list(self.dependency_ids))
 
+    @api.model
     def action_make_applist_from_local_instance(self):
         for x in map(self.browse, self._search([])):
             x.unlink()
